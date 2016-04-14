c0d270e93e686db6919adb4b5d9a5901cd1eb022
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-04-14 22:08:21 +0200

    Fix typo

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,11 +1,8 @@
 describe('Address Book', function() {
 	it('should be able to add a contact', function () {
-
-		var adressBook = new AddressBook(),
-			thisContact = new Contact();
+		var addressBook = new AddressBook(), thisContact = new Contact();
 
 		addressBook.addContact(thisContact);
-
 		expect(addressBook.getContact(0)).toBe(thisContact);
 
 	});
