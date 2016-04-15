cd8e7c411bf3890c672cd8209c063ab2670818e9
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-04-15 08:46:24 +0200

    Update AddressBook.js to pass test to add and get contacts

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,3 +1,11 @@
-var AddressBook() {
-	
-};
+function AddressBook() {
+	this.contacts = [];
+}
+
+AddressBook.prototype.addContact = function(contact) {
+	this.contacts.push(contact);
+}
+
+AddressBook.prototype.getContact = function(index) {
+	return this.contacts[index];
+}
