1825ec5242ac5c5ff8208cb339b18871bdf35910
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-04-15 10:12:26 +0200

    Add asynchronous test

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,15 +1,29 @@
 function AddressBook() {
 	this.contacts = [];
-}
+	this.initialComplete = false;
+};
+
+
+//Asynchronous request - fake API call.
+AddressBook.prototype.getInitialContacts = function(callback) {
+	var self = this;
+
+	setTimeout(function() {
+		self.initialComplete = true;
+		if (callback) {
+			return callback();
+		}
+	}, 3);
+};
 
 AddressBook.prototype.addContact = function(contact) {
 	this.contacts.push(contact);
-}
+};
 
 AddressBook.prototype.getContact = function(index) {
 	return this.contacts[index];
-}
+};
 
 AddressBook.prototype.deleteContact = function(index) {
 	this.contacts.splice(index, 1);
-}
+};
